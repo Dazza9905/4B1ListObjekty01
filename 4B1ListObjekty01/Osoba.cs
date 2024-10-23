30708fe071c9e23a10d3350614b7289826a1ff30
Daren Drahos <Daren.Drahos@spsehalova.sk>
2024-10-23 10:25:55 +0200

    Add project files.

---
+++ 4B1ListObjekty01/Osoba.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4B1ListObjekty01
+{
+    class Osoba
+    {
+        private int pocet;
+        private int vek;
+
+        public Osoba(int pocet, int vek)
+        {
+            SetPocet(pocet);
+            SetVek(vek);
+        }
+
+        public Osoba()
+        {
+            SetPocet(0);
+            SetVek(0);
+        }
+
+
+
+        public void SetPocet(int pocet)
+        {
+            this.pocet = pocet;
+        }
+        public int GetPocet()
+        {
+            return pocet;
+        }
+
+        public void SetVek(int vek)
+        {
+            this.vek = vek;
+        }
+        public int GetVek()
+        {
+            return vek;
+        }
+    }
+}
---
+++ 4B1ListObjekty01/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4B1ListObjekty01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Osoba sportovec = new Osoba();
+
+            List<Osoba> sportovci = new List<Osoba>();
+            sportovci.Add(sportovec);
+
+            foreach (Osoba osoba in sportovci)
+            {
+                Console.Write(osoba.GetPocet());
+            }
+            //ALEBO
+            for (int i = 0; i < sportovci.Count; i++)
+            {
+                Console.Write(sportovci[i].GetPocet());
+            }
+
+            sportovci[0].SetPocet(70);
+
+            Console.WriteLine(sportovci[0].GetPocet());
+
+            sportovci[0].SetVek(18);
+            Console.WriteLine(sportovci[0].GetVek());
+
+            Osoba sportovec2 = new Osoba();
+            sportovci.Add(sportovec2);
+            sportovci[1].SetPocet(100);
+            sportovci[1].SetVek(22);
+            Console.WriteLine(sportovci[1].GetPocet());
+            Console.WriteLine(sportovci[1].GetVek());
+
+            //zapis 20 objektov do sportovci
+            Random gen = new Random();
+            for (int i = 0; i < 20; i++)
+            {
+                sportovci.Add(new Osoba(gen.Next(50, 201), gen.Next(16, 26)));
+            }
+
+            foreach (Osoba osoba in sportovci)
+            {
+                Console.WriteLine(osoba.GetPocet() + "\t" + osoba.GetVek());
+
+            }
+
+            Console.Read();
+        }
+    }
+}
